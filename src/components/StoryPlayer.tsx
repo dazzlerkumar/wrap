10cda17fd94061f190de1af7b3673410eb0d63be
dazzlerkumar <dazzler.deepak98@gmail.com>
2026-01-02 12:54:13 +0530

    refactor(storyPlayer): width fixes

--- src/components/StoryPlayer.tsx
+++ src/components/StoryPlayer.tsx
@@ -142,7 +142,7 @@ export default function StoryPlayer() {
 
             {/* Main Story Frame */}
             <div
-                className="relative z-20 flex aspect-[9/16] h-full max-h-[900px] w-full flex-col overflow-hidden bg-black shadow-2xl md:h-[min(90vh,900px)] md:rounded-2xl border border-white/10"
+                className="relative z-20 flex aspect-[9/16] h-full max-h-[900px] w-full flex-col overflow-hidden bg-black shadow-2xl md:w-[400px] md:h-[min(95vh,900px)] md:rounded-2xl border border-2 border-white/10"
                 onPointerDown={handlePointerDown}
                 onPointerUp={handlePointerUp}
                 onPointerCancel={handlePointerUp}
